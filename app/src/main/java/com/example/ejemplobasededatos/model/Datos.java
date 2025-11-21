c74be92b78d026d29bf53fbbaf0d2c6e8f14065e
LeOn-Opmac <leoncampo2707@gmail.com>
2025-11-21 12:23:21 -0500

    Manager

--- app/src/main/java/com/example/ejemplobasededatos/model/Datos.java
+++ app/src/main/java/com/example/ejemplobasededatos/model/Datos.java
@@ -1,4 +1,37 @@
 package com.example.ejemplobasededatos.model;
 
 public class Datos {
+    private String nombre;
+    private String apellido;
+    private String correo;
+
+    public Datos(String nombre, String apellido, String correo){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+    }
+
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public String getApellido(){
+        return apellido;
+    }
+    public String getCorreo(){
+        return correo;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido){
+        this.apellido = apellido;
+    }
+
+    public void setCorreo(String correo){
+        this.correo = correo;
+    }
 }
--- app/src/main/java/com/example/ejemplobasededatos/model/Manager.java
+++ app/src/main/java/com/example/ejemplobasededatos/model/Manager.java
@@ -1,4 +1,25 @@
 package com.example.ejemplobasededatos.model;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.widget.Toast;
+
 public class Manager {
+
+    private ConexionBd conexionBd;
+    private SQLiteDatabase db;
+
+    public Manager(Context context){
+        conexionBd = new ConexionBd(context);
+    }
+
+    public void openBdWr(){
+        // abre la base de datos en modo escritura
+        db = conexionBd.getWritableDatabase();
+    }
+
+    public void openDbRd(){
+        // abre la base de datos en modo lectura
+        db = conexionBd.getReadableDatabase();
+    }
 }
